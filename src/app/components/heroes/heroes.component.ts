56cc20bc1b06df68611e762113e58eebad6e4ecd
Edier Bernal Rozo <edier.bernal@outlook.com>
2022-07-07 18:17:03 -0500

    creacion-rutas-heroes-to-heroe-with-routerlink

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
+
 export class HeroesComponent implements OnInit {
   heroes: heroe[] = [];
-  constructor(private _heroesService: HeroesService) {}
+
+  constructor(private _heroesService: HeroesService, private _router : Router) { }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
+    // console.log(this.heroes);
   }
+
+  verHeroe(index: number) {
+    console.log(index);
+    this._router.navigate(['/heroe/',index]);
+  }
+
 }
